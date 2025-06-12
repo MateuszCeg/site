8f48ee62b56ca7b220a6056ef800e0dc895e5b4f
student <asbc@s.com>
2025-06-12 16:23:57 +0200

    lab2

--- src/main/java/ImageHandler.java
+++ src/main/java/ImageHandler.java
@@ -1,40 +1,150 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ImageHandler {
-    public ImageHandler(BufferedImage bufferedImage) {
-        this.bufferedImage = bufferedImage;
-    }
-    private BufferedImage bufferedImage;
-    public void loadImage(String filePath) {
-        File file = new File(filePath);
-        try {
-            bufferedImage = ImageIO.read(file);
-        } catch (IOException e) {
-            System.err.println("Zła ścieżka: " + filePath);
+    private BufferedImage image;
+    private class AdjustBrightnessByThreads extends Thread {
+        private int start;
+        private int end;
+        private int value;
+
+        public AdjustBrightnessByThreads(int start, int end, int value) {
+            this.start = start;
+            this.end = end;
+            this.value = value;
         }
-        System.out.println("Załadowano obraz.");
-    }
-    public void saveImage(String filePath){
-        String extension = filePath.substring(filePath.lastIndexOf("."));
-        try {
-            ImageIO.write(bufferedImage, extension, new File(filePath));
-        } catch (IOException e) {
-            System.err.println("Błąd zapisu");
-        }
-        System.out.println("Zapisano obraz.");
-    }
-    public void setBrightness(int number){
-        for (int x = 0; x < bufferedImage.getWidth(); x++) {
-            for (int y = 0; y < bufferedImage.getHeight(); y++) {
-                int rgbOfPixel = bufferedImage.getRGB(x,y);
-                if (rgbOfPixel+number>255) bufferedImage.setRGB(x,y,255);
-                else if (rgbOfPixel+number<0) bufferedImage.setRGB(x,y,0);
-                else bufferedImage.setRGB(x,y,rgbOfPixel+number);
+
+        @Override
+        public void run() {
+            // Iteracja po pikselach obrazu i zmiana jasności
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = start; y < end; y++) {
+                    int rgb = image.getRGB(x, y);
+                    int alpha = (rgb >> 24) & 0xFF;
+                    int red = (rgb >> 16) & 0xFF;
+                    int green = (rgb >> 8) & 0xFF;
+                    int blue = rgb & 0xFF;
+
+                    // Zmiana jasności dla każdego kanału koloru
+                    red = clamp(red + value);
+                    green = clamp(green + value);
+                    blue = clamp(blue + value);
+
+                    int newRGB = (alpha << 24) | (red << 16) | (green << 8) | blue;
+                    image.setRGB(x, y, newRGB);
+                }
             }
         }
     }
+
+    // Metoda do wczytania obrazu z podanej ścieżki
+    public void loadImage(String path) {
+        try {
+            File file = new File(path);
+            image = ImageIO.read(file);
+            System.out.println("Obraz został wczytany pomyślnie.");
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd podczas wczytywania obrazu: " + e.getMessage());
+        }
+    }
+
+    // Metoda do zapisania obrazu z pola klasy do podanej ścieżki
+    public void saveImage(String path) {
+        try {
+            File file = new File(path);
+            String format = path.substring(path.lastIndexOf('.') + 1);
+            ImageIO.write(image, format, file);
+            System.out.println("Obraz został zapisany pomyślnie.");
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd podczas zapisywania obrazu: " + e.getMessage());
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Niepoprawna ścieżka, brak rozszerzenia obrazu.");
+        }catch (IllegalArgumentException e) {
+            System.out.println("Brak wczytanego obrazu do zapisania.");
+        }
+    }
+
+    // Metoda do pobrania obiektu BufferedImage (jeśli potrzebne)
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    // Metoda do zwiększenia jasności obrazu o podaną stałą
+    public void adjustBrightness(int value) {
+        if (image == null) {
+            System.out.println("Brak wczytanego obrazu.");
+            return;
+        }
+
+        // Iteracja po pikselach obrazu i zmiana jasności
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                int rgb = image.getRGB(x, y);
+                int alpha = (rgb >> 24) & 0xFF;
+                int red = (rgb >> 16) & 0xFF;
+                int green = (rgb >> 8) & 0xFF;
+                int blue = rgb & 0xFF;
+
+                // Zmiana jasności dla każdego kanału koloru
+                red = clamp(red + value);
+                green = clamp(green + value);
+                blue = clamp(blue + value);
+
+                int newRGB = (alpha << 24) | (red << 16) | (green << 8) | blue;
+                image.setRGB(x, y, newRGB);
+            }
+        }
+
+        //graphics.dispose();
+    }
+        public void adjustBrightnessThreads(int value, int threads) throws InterruptedException {
+            if (image == null) {
+                System.out.println("Brak wczytanego obrazu.");
+                return;
+            }
+            Thread[] threadsTable = new Thread[threads];
+            int start; int end; int high = image.getHeight()/threads;
+            for (int i = 0; i < threads; i++) {
+                start=i*high;
+                if (i+1 == threads) end = image.getHeight();
+                else end = start+high;
+                threadsTable[i] = new AdjustBrightnessByThreads(start,end,value);
+                threadsTable[i].start();
+            }
+            for (int i = 0; i < threads; i++) {
+                threadsTable[i].join();
+            }
+        }
+        public void adjustBrightnessPoolThreads(int value) {
+            if (image == null) {
+                System.out.println("Brak wczytanego obrazu.");
+                return;
+            }
+            ExecutorService executorService = Executors.newFixedThreadPool(image.getHeight());
+            for (int i = 0; i < image.getHeight(); i++) {
+                executorService.execute(new AdjustBrightnessByThreads(i,i+1,value));
+            }
+            executorService.shutdown();
+            try {
+                executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    // Metoda pomocnicza do ograniczenia wartości do zakresu 0-255
+    private int clamp(int value) {
+        return Math.min(Math.max(0, value), 255);
+    }
+
+
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        ImageHandler newImage = new ImageHandler(null);
-        newImage.loadImage("src/main/resources/testImage.png");
-        newImage.setBrightness(10);
-        newImage.saveImage("CopyOfImage.png");
+        ImageHandler handler = new ImageHandler();
+        //handler.saveImage("obrazKopia.jpg");
+
+        // Wczytanie obrazu
+        handler.loadImage("src/main/java/obraz.jpg");
+
+        // Zwiększenie jasności obrazu o 100
+        long startTimeParallel = System.currentTimeMillis();
+        handler.adjustBrightness(100);
+        long endTimeParallel = System.currentTimeMillis();
+        System.out.println("Czas wykonania jeden wątek: " + (endTimeParallel - startTimeParallel) + " ms");
+
+        handler.saveImage("src/main/java/obrazKopia1.jpg");
+
+
+        try {
+            handler.loadImage("src/main/java/obraz.jpg");
+            long startTimeParallel2 = System.currentTimeMillis();
+            handler.adjustBrightnessThreads(100, Runtime.getRuntime().availableProcessors());
+            long endTimeParallel2 = System.currentTimeMillis();
+            System.out.println("Czas wykonania jeden wątek: " + (endTimeParallel2 - startTimeParallel2) + " ms");
+            handler.saveImage("src/main/java/obrazKopia2.jpg");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        handler.loadImage("src/main/java/obraz.jpg");
+        long startTimeParallel3 = System.currentTimeMillis();
+        handler.adjustBrightnessPoolThreads(100);
+        long endTimeParallel3 = System.currentTimeMillis();
+        System.out.println("Czas wykonania jeden wątek: " + (endTimeParallel3 - startTimeParallel3) + " ms");
+        handler.saveImage("src/main/java/obrazKopia3.jpg");
     }
 }
